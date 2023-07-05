b9f52d96b422d5ff54ce6bd55b7838101d51eb75
KirillTkachev <kirillkha92@gmail.com>
2023-07-05 12:15:25 +0200

    Update sender_summaries.py

--- backend_app/sender_summaries.py
+++ backend_app/sender_summaries.py
@@ -13,6 +13,7 @@ backend_ip = os.getenv('backend_ip')
 base_url = f"http://{backend_ip}:8008/api/summary/create/"
 base_youtube_url = "https://www.youtube.com/watch?v="
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', help='input folder')
@@ -26,6 +27,7 @@ def send_request(link_to_video: str, s3_key, type: str):
          's3_key': s3_key, 'type': type, 'votes': 0})
     return response
 
+
 def find_between( s, first, last ):
     try:
         start = s.index( first ) + len( first )
@@ -33,6 +35,7 @@ def find_between( s, first, last ):
         return s[start:end]
     except ValueError:
         return ""
+
 
 def main():
     args = parse_args()
@@ -51,6 +54,7 @@ def main():
         response = send_request(link_to_video=(base_youtube_url + youtube_id), \
             s3_key=key, type=type)
         print(response)
+
 
 if __name__ == '__main__':
     main()
