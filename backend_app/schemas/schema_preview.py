5f91a5e12c87d070c7d7714d8890d35fa259e010
KirillTkachev <kirillkha92@gmail.com>
2023-07-05 12:22:46 +0200

    Update schema_preview.py

--- backend_app/schemas/schema_preview.py
+++ backend_app/schemas/schema_preview.py
@@ -1,11 +1,13 @@
 import datetime as _dt
 import pydantic as _pydantic
 
+
 class _BasePreview(_pydantic.BaseModel):
     type: str
     link_to_video: str
     s3_key: str
     votes: int
+
 
 class Preview(_BasePreview):
     id: int
@@ -13,5 +15,7 @@ class Preview(_BasePreview):
     class Config:
         orm_mode = True
 
+
 class CreatePreview(_BasePreview):
     pass
+    
