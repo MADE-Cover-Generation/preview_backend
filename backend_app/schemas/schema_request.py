6c967d90e1963bf865592f4ebd938653918333dc
KirillTkachev <kirillkha92@gmail.com>
2023-07-05 12:22:59 +0200

    Update schema_request.py

--- backend_app/schemas/schema_request.py
+++ backend_app/schemas/schema_request.py
@@ -1,15 +1,18 @@
 import datetime as _dt
 import pydantic as _pydantic
 
+
 class _BaseRequest(_pydantic.BaseModel):
     telegram_id: str
     link_to_video: str
+
 
 class Request(_BaseRequest):
     id: int
 
     class Config:
         orm_mode = True
+
 
 class CreateRequest(_BaseRequest):
     pass
