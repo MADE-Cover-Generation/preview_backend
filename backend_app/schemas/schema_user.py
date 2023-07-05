cd8d00ba15f4f754d53c1ea3337ff65a61d1b3b5
KirillTkachev <kirillkha92@gmail.com>
2023-07-05 12:23:23 +0200

    Update schema_user.py

--- backend_app/schemas/schema_user.py
+++ backend_app/schemas/schema_user.py
@@ -1,15 +1,18 @@
 import datetime as _dt
 import pydantic as _pydantic
 
+
 class _BaseUser(_pydantic.BaseModel):
     telegram_id: str
     watched_ids: list
+
 
 class User(_BaseUser):
     id: int
 
     class Config:
         orm_mode = True
+
 
 class CreateUser(_BaseUser):
     pass
