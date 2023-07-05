43620c58f644f44d0b0505778dcfd1516e695674
KirillTkachev <kirillkha92@gmail.com>
2023-07-05 12:23:10 +0200

    Update schema_summary.py

--- backend_app/schemas/schema_summary.py
+++ backend_app/schemas/schema_summary.py
@@ -1,17 +1,20 @@
 import datetime as _dt
 import pydantic as _pydantic
 
+
 class _BaseSummary(_pydantic.BaseModel):
     type: str
     link_to_video: str
     s3_key: str
     votes: int
 
+
 class Summary(_BaseSummary):
     id: int
 
     class Config:
         orm_mode = True
+
 
 class CreateSummary(_BaseSummary):
     pass
